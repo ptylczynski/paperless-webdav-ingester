856b2220f0edbb9cf9bd3f93a089233bfde97e6a
ptylczynski <piotr@ptl.cloud>
2022-02-18 12:45:17 +0100

    init

---
+++ src/main/java/cloud/ptl/paperlesswebdavingester/PaperlessWebdavIngesterApplication.java
@@ -0,0 +1,12 @@
+package cloud.ptl.paperlesswebdavingester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperlessWebdavIngesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperlessWebdavIngesterApplication.class, args);
+    }
+}
---
+++ src/test/java/cloud/ptl/paperlesswebdavingester/PaperlessWebdavIngesterApplicationTests.java
@@ -0,0 +1,12 @@
+package cloud.ptl.paperlesswebdavingester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessWebdavIngesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
