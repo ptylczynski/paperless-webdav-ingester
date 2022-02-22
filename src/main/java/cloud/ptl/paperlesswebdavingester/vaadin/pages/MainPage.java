1d66f6ef6a93e4851ab339a27536cbbb04e4b13a
ptylczynski <piotr@ptl.cloud>
2022-02-22 21:49:25 +0100

    change: add button to main page

--- src/main/java/cloud/ptl/paperlesswebdavingester/vaadin/pages/MainPage.java
+++ src/main/java/cloud/ptl/paperlesswebdavingester/vaadin/pages/MainPage.java
@@ -1,27 +1,28 @@
 package cloud.ptl.paperlesswebdavingester.vaadin.pages;
 
-import com.vaadin.flow.component.Key;
+import cloud.ptl.paperlesswebdavingester.ingester.services.IngestionService;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Route("")
 public class MainPage extends VerticalLayout {
 
-    public MainPage() {
-        VerticalLayout todosList = new VerticalLayout();
-        TextField taskField = new TextField();
-        Button addButton = new Button("Add");
-        addButton.addClickListener(click -> {
-            Checkbox checkbox = new Checkbox(taskField.getValue());
-            todosList.add(checkbox);
-        });
-        addButton.addClickShortcut(Key.ENTER);
+    private final IngestionService ingestionService;
 
-        add(new H1("Vaadin Todo"), todosList, new HorizontalLayout(taskField, addButton));
+    public MainPage(IngestionService ingestionService) {
+        this.ingestionService = ingestionService;
+        Button btn = new Button("start");
+        btn.addClickListener(e -> {
+            try {
+                ingestionService.startIngest();
+            } catch (IOException | URISyntaxException ex) {
+                ex.printStackTrace();
+            }
+        });
+        add(btn);
     }
 }
