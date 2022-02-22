c389db47f0e8c5fb990758aa360b17c3857d4e20
ptylczynski <piotr@ptl.cloud>
2022-02-22 21:50:43 +0100

    change: create webdav client from sardine as bean ready to inject

--- src/main/java/cloud/ptl/paperlesswebdavingester/PaperlessWebdavIngesterApplication.java
+++ src/main/java/cloud/ptl/paperlesswebdavingester/PaperlessWebdavIngesterApplication.java
@@ -1,12 +1,27 @@
 package cloud.ptl.paperlesswebdavingester;
 
+import com.github.sardine.Sardine;
+import com.github.sardine.SardineFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PaperlessWebdavIngesterApplication {
+    @Value("${webdav.username}")
+    private String username;
+    @Value("${webdav.password}")
+    private String password;
 
     public static void main(String[] args) {
         SpringApplication.run(PaperlessWebdavIngesterApplication.class, args);
+    }
+
+    @Bean
+    public Sardine createClient() {
+        return SardineFactory.begin(username, password);
     }
 }
