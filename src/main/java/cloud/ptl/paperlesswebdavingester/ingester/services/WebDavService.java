77815d70ad9535257582f8ba3b8a316bd2e5220c
ptylczynski <piotr@ptl.cloud>
2022-02-22 21:51:40 +0100

    change: modify WebDavService

--- src/main/java/cloud/ptl/paperlesswebdavingester/ingester/services/WebDavService.java
+++ src/main/java/cloud/ptl/paperlesswebdavingester/ingester/services/WebDavService.java
@@ -2,39 +2,59 @@ package cloud.ptl.paperlesswebdavingester.ingester.services;
 
 import com.github.sardine.DavResource;
 import com.github.sardine.Sardine;
-import com.github.sardine.SardineFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 @Component
+@Slf4j
 public class WebDavService {
 
-    private final StorageService storageService;
-    @Value("${webdav.username}")
-    private String username;
-    @Value("${webdav.password}")
-    private String password;
+    private final LocalStorageService storageService;
+    private final Sardine webDavClient;
+    @Value("${webdav.host}")
+    private String host;
 
-    public WebDavService(StorageService storageService) {
+    public WebDavService(LocalStorageService storageService, Sardine webDavClient) {
+        this.webDavClient = webDavClient;
         this.storageService = storageService;
     }
 
-    private Sardine createClient() {
-        return SardineFactory.begin(username, password);
+    private String assembleFullPath(String path) throws MalformedURLException, URISyntaxException {
+        // remove last slash if added
+        if (host.endsWith("/")) {
+            host = host.substring(0, host.length() - 1);
+        }
+        if (path.startsWith("/")) {
+            path = path.substring(1);
+        }
+        // encode to proper url, otherwise spaces could break protocol
+        URL url = new URL(host + "/" + path);
+        return new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(),
+                url.getQuery(), url.getRef()).toASCIIString();
     }
 
-    public List<DavResource> list(String path) throws IOException {
-        return createClient().list(path);
+    public List<DavResource> list(DavResource resource) throws IOException, URISyntaxException {
+        return list(resource.getPath());
     }
 
-    public File get(String path) throws IOException {
-        Sardine sardine = createClient();
-        InputStream inputStream = sardine.get(path);
-        return storageService.save(inputStream, path);
+    public List<DavResource> list(String path) throws IOException, URISyntaxException {
+        return webDavClient.list(assembleFullPath(path));
+    }
+
+    public InputStream get(DavResource resource) throws IOException, URISyntaxException {
+        return get(resource.getPath());
+    }
+
+    public InputStream get(String path) throws IOException, URISyntaxException {
+        return webDavClient.get(assembleFullPath(path));
     }
 }
