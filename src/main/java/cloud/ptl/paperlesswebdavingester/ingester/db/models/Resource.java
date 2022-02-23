feeeec9378943e6aeb37eed576dcdb46c3212a1b
ptylczynski <piotr@ptl.cloud>
2022-02-23 01:19:18 +0100

    change: remove File field from Resource, add getFileName method

--- src/main/java/cloud/ptl/paperlesswebdavingester/ingester/db/models/Resource.java
+++ src/main/java/cloud/ptl/paperlesswebdavingester/ingester/db/models/Resource.java
@@ -1,9 +1,12 @@
 package cloud.ptl.paperlesswebdavingester.ingester.db.models;
 
 import lombok.Data;
+import org.apache.commons.codec.digest.DigestUtils;
 
-import javax.persistence.*;
-import java.io.File;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "file")
@@ -12,15 +15,18 @@ public class Resource {
     @Id
     @GeneratedValue
     private Long id;
+    private Long paperlessId;
     private String externalPath;
     private String internalPath;
-    @Transient
-    private File file;
+    private Boolean isLocalCopyPresent;
     // generated on webdav side
     private String etag;
 
-    @PostLoad
-    public void init() {
-        file = new File(internalPath);
+    public String getHashedExternalPath() {
+        return DigestUtils.sha256Hex(externalPath);
+    }
+
+    public String getFileName() {
+        return internalPath.substring(internalPath.lastIndexOf("/") + 1);
     }
 }
