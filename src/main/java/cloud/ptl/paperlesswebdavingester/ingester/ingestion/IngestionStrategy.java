f461a8432747d768b172e1fc0867ffc355d35690
ptylczynski <piotr@ptl.cloud>
2022-02-28 22:41:51 +0100

    change: update IngestionStrategy and move to new location

--- src/main/java/cloud/ptl/paperlesswebdavingester/ingester/ingestion/IngestionStrategy.java
+++
@@ -1,9 +0,0 @@
-package cloud.ptl.paperlesswebdavingester.ingester.ingestion;
-
-import java.util.Map;
-
-public interface IngestionStrategy {
-    boolean supports(IngestionMode ingestionMode);
-    void ingest(Map<Object, Object> params) throws IngestionException;
-    void ingest() throws IngestionException;
-}
