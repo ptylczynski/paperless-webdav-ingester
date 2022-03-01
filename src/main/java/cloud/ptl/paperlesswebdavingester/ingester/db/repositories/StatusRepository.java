3215d5de0b276c1b1fbdc8f44ddd38d8b45befa0
ptylczynski <piotr@ptl.cloud>
2022-03-01 15:35:26 +0100

    change: add StatusRepository::findAllByIsRunningEquals

--- src/main/java/cloud/ptl/paperlesswebdavingester/ingester/db/repositories/StatusRepository.java
+++ src/main/java/cloud/ptl/paperlesswebdavingester/ingester/db/repositories/StatusRepository.java
@@ -3,6 +3,9 @@ package cloud.ptl.paperlesswebdavingester.ingester.db.repositories;
 import cloud.ptl.paperlesswebdavingester.ingester.db.models.Status;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StatusRepository extends CrudRepository<Status, Long> {
     boolean existsByIsRunningEquals(boolean running);
+    List<Status> findAllByIsRunningEquals(boolean running);
 }
