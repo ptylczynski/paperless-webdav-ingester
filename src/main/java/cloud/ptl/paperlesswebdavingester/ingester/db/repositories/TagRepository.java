0322088626bcae1895ae24afe81126b2f6a19e15
ptylczynski <piotr@ptl.cloud>
2022-03-15 23:30:20 +0100

    change: add TagRepository::findByPaperlessId

--- src/main/java/cloud/ptl/paperlesswebdavingester/ingester/db/repositories/TagRepository.java
+++ src/main/java/cloud/ptl/paperlesswebdavingester/ingester/db/repositories/TagRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TagRepository extends CrudRepository<Tag, Long> {
+    Optional<Tag> findByPaperlessId(Long paperlessId);
     List<Tag> findAllByNameIn(List<String> names);
     Optional<Tag> findByName(String name);
 }
