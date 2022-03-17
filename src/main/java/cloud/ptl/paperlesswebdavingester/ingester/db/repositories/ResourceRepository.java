3808a0a3c845270a84f5636f34bce366d41c8835
ptylczynski <piotr@ptl.cloud>
2022-03-17 21:07:21 +0100

    change: add ResourceRepository::findAllByIngestedIn_IngestionMode method

--- src/main/java/cloud/ptl/paperlesswebdavingester/ingester/db/repositories/ResourceRepository.java
+++ src/main/java/cloud/ptl/paperlesswebdavingester/ingester/db/repositories/ResourceRepository.java
@@ -1,11 +1,14 @@
 package cloud.ptl.paperlesswebdavingester.ingester.db.repositories;
 
 import cloud.ptl.paperlesswebdavingester.ingester.db.models.Resource;
+import cloud.ptl.paperlesswebdavingester.ingester.ingestion.IngestionMode;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ResourceRepository extends CrudRepository<Resource, Long> {
+    List<Resource> findAllByIngestedIn_IngestionMode(IngestionMode ingestionMode);
     Optional<Resource> findByPaperlessId(Long paperlessId);
     Optional<Resource> findByExternalPath(String externalPath);
     boolean existsByInternalPath(String internalPath);
